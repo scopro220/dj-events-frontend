c648d734f52163e6d730b73cd5b86f56ac48f35b
Scott Provence <scott.provence@yahoo.com>
2021-11-19 13:19:13 -0500

    Connect to Strapi API

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,6 +4,7 @@ import EventItem from "@/components/EventItem";
 import { API_URL } from "@/config/index";
 
 export default function HomePage({ events }) {
+  console.log(events);
   return (
     <Layout>
       <h1>Upcoming Events</h1>
@@ -23,11 +24,11 @@ export default function HomePage({ events }) {
 }
 
 export async function getStaticProps() {
-  const res = await fetch(`${API_URL}/api/events`);
+  const res = await fetch(`${API_URL}/events?_sort=date:ASC&_limit=3`);
   const events = await res.json();
 
   return {
-    props: { events: events.slice(0, 3) },
+    props: { events },
     revalidate: 1,
   };
 }
