0d3a5ec1d365cbd401614f390a2186cdf6f5123b
Scott Provence <scott.provence@yahoo.com>
2021-11-20 13:40:52 -0500

    Store JWT in server httponly cookie

--- context/AuthContext.jsx
+++ context/AuthContext.jsx
@@ -30,8 +30,6 @@ export const AuthProvider = ({ children }) => {
 
     const data = await res.json();
 
-    console.log(data);
-
     if (res.ok) {
       setUser(data.user);
     } else {
--- pages/api/login.js
+++ pages/api/login.js
@@ -1,3 +1,4 @@
+import cookie from "cookie";
 import { API_URL } from "@/config/index";
 
 /* eslint import/no-anonymous-default-export: [2, {"allowArrowFunction": true}] */
@@ -18,10 +19,18 @@ export default async (req, res) => {
 
     const data = await strapiRes.json();
 
-    console.log(data.jwt);
-
     if (strapiRes.ok) {
       // Set Cookie
+      res.setHeader(
+        "Set-Cookie",
+        cookie.serialize("token", data.jwt, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV !== "development",
+          maxAge: 60 * 60 * 24 * 7, // 1 week
+          sameSite: "strict",
+          path: "/",
+        })
+      );
       res.status(200).json({ user: data.user });
     } else {
       res
